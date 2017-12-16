1f9ea93d93e338d5ad63128d26c180dc3948cbfc
Dennis Hamilton <orcmid@apache.org>
2017-12-16 08:09:09 -0800

    1.0.1 script.js Suppress Search Functions
    The search box and form are removed from the .ejs.  The related js
    functions are suppressed to ensure no hiccups

--- source/js/script.js
+++ source/js/script.js
@@ -1,35 +1,36 @@
 (function($){
   // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+  // 1.0.1 Remove Search Support
+  //* var $searchWrap = $('#search-form-wrap'),
+  //*   isSearchAnim = false,
+  //*   searchAnimDuration = 200;
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
+  //* var startSearchAnim = function(){
+  //*   isSearchAnim = true;
+  //* };
 
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
+  //* var stopSearchAnim = function(callback){
+  //*   setTimeout(function(){
+  //*     isSearchAnim = false;
+  //*     callback && callback();
+  //*   }, searchAnimDuration);
+  //* };
 
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
+  //* $('#nav-search-btn').on('click', function(){
+  //*   if (isSearchAnim) return;
 
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
-  });
+  //*   startSearchAnim();
+  //*   $searchWrap.addClass('on');
+  //*   stopSearchAnim(function(){
+  //*     $('.search-form-input').focus();
+  //*   });
+  //* });
 
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
+  //* $('.search-form-input').on('blur', function(){
+  //*   startSearchAnim();
+  //*   $searchWrap.removeClass('on');
+  //*   stopSearchAnim();
+  //* });
 
   // Share
   $('body').on('click', function(){
@@ -135,3 +136,6 @@
     $container.removeClass('mobile-nav-on');
   });
 })(jQuery);
+
+// 1.0.1 2017-12-16-08:03 Disable search-dependent definitions for wingnut
+// 1.0.0 2017-12-11-13:37 Original script of landscape 0.0.2 wingnut refresh
